01aca7a7b2a2e36b477539b51bc20b309a60f1e9
RobinPater <400018894@st.roc.a12.nl>
2020-12-15 12:13:26 +0100

    adminpagetable

--- Produblog/admin_home.php
+++ Produblog/admin_home.php
@@ -1,9 +1,50 @@
 <?php
 require_once('assets/includes/include.php');
 
+$con = getDBConnection();
+
   if(isset($_SESSION['usertype']) && $_SESSION['usertype'] == 'admin'){
 	echo"
-	hoi
+		
+		<div class='table-responsive'>
+		
+		"; 
+				
+			$query = "SELECT * FROM users";
+			$query_run = $con->prepare($query);
+		
+		echo"
+		
+			<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'>
+				<thead>
+					<tr>
+						<th>User id</th>
+						<th>Username</th>
+						<th>Email</th>
+						<th>Usertype</th>
+						<th>EDIT</th>
+						<th>DELETE</th>
+					</tr>
+				</thead>
+				<tbody>
+			";
+			
+			if()
+			
+			echo"
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					
+				</tbody>
+			</table>
+		</div>
+	
 	";
 
 }else{
@@ -11,3 +52,4 @@ require_once('assets/includes/include.php');
 }
 
 ?>
+
