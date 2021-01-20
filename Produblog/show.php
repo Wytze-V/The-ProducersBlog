425ed4cff5b5a15f0abc2af1cf9957f95240de36
RobinPater <400018894@st.roc.a12.nl>
2021-01-20 15:27:11 +0100

    comment

--- Produblog/show.php
+++ Produblog/show.php
@@ -27,28 +27,36 @@ $id = $_GET["id"];
 					<p>".$post->postcontent."</p>
 					<p><button class='readbtn'><a href='post_view.php'>Go Back</a></button></p>
 
-					
-					</div>              
-		
 					<div class='col-lg-3'>
 						<a href=''> Comment (0)</a>
 					</div>
-					
+
 					<div class='row'>
 						<div class='col-lg-4'></div>
 						<div class='col-lg-6'>
 							<form class='form-horizontal' action='ac_comment.php'  method='POST'>
 								<div class='form-group'>
-								<label class='col-lg-3 control label'>Add</label>
+									<label class='col-lg-3 control-label'>Add Comment</label>
+									<div class='col-lg-9'>
+										<textarea class='form-control' rows='5' cols='10' name='comment' placeholde='Comment'></textarea>
+									</div>
 								</div>
+								<input type='submit' name='postcomment' value='Comment' class='btn btn-primary'>
+								<a href='home.php' class='btn btn-default'>Go home</a>
 							</form>
 				
 						</div>
 					</div>
+					
+					</div>              
+		
+
+					
+
 		
 			</div>
 			
-				
+
 
 			
 			
